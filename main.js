35eb30afaac043aec3d26628b8b7dea9a6a412d3
Ethan Groene <ethangroene@gmail.com>
2022-08-08 00:11:06 -0500

    Correct image paths, add styling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Function to set background upon loading of page:
 const setInitialBackground = () => {
-    document.body.style.background = 'url(./assets/initial-bg)';
+    document.body.style.background = 'url(./assets/initial-bg.jpg)';
+    document.body.style.backgroundSize = 'cover';
 }
 
 // Initialize quiz score at 0:
@@ -15,7 +16,8 @@ const statesInfo = [
 // Add a function for each state (as the correct answer will be in a different place in the order of options) that hides the previous state's function & displays the current one and updates the point total accordingly.
 // First function should hide #greeting-box; last function should display a button 'get results', which, upon click, will display a new box with the score & hide #total-score
 const alabama = () => {
-    document.body.style.background = 'url(./assets/al-bg)';
+    document.body.style.background = 'url(./assets/al-bg.jpg';
+    document.body.style.backgroundSize = 'cover';
     document.getElementById('greeting-box').style.display = 'none';
 
 }
