21defd60f582b0e4d7c0d0b2ca2e0739d0e736dd
Ethan Groene <ethangroene@gmail.com>
2022-08-08 00:01:28 -0500

    Set init BG w/ function; start AL function

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// Function to set background upon loading of page:
+const setInitialBackground = () => {
+    document.body.style.background = 'url(./assets/initial-bg)';
+}
+
 // Initialize quiz score at 0:
 let totalScore = 0;
 
@@ -6,3 +11,11 @@ const statesInfo = [
     { stateName: "Alabama", stateCapital: "Montgomery", wrongOptionOne: "Tuscaloosa", wrongOptionTwo: "Mobile", wrongOptionThree: "Huntsville" },
     { stateName: "Alaska", stateCapital: "Juneau", wrongOptionOne: "Anchorage", wrongOptionTwo: "Barrow", wrongOptionThree: "Naknek" }
 ]
+
+// Add a function for each state (as the correct answer will be in a different place in the order of options) that hides the previous state's function & displays the current one and updates the point total accordingly.
+// First function should hide #greeting-box; last function should display a button 'get results', which, upon click, will display a new box with the score & hide #total-score
+const alabama = () => {
+    document.body.style.background = 'url(./assets/al-bg)';
+    document.getElementById('greeting-box').style.display = 'none';
+
+}
