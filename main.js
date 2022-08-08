7abd40a52dd9b81cc891875bec4fe6f1a31db872
Ethan Groene <ethangroene@gmail.com>
2022-08-08 01:21:59 -0500

    Remove AL function to be added again after creating new branch

--- main.js
+++ main.js
@@ -12,12 +12,3 @@ const statesInfo = [
     { stateName: "Alabama", stateCapital: "Montgomery", wrongOptionOne: "Tuscaloosa", wrongOptionTwo: "Mobile", wrongOptionThree: "Huntsville" },
     { stateName: "Alaska", stateCapital: "Juneau", wrongOptionOne: "Anchorage", wrongOptionTwo: "Barrow", wrongOptionThree: "Naknek" }
 ]
-
-// Add a function for each state (as the correct answer will be in a different place in the order of options) that hides the previous state's function & displays the current one and updates the point total accordingly.
-// First function should hide #greeting-box; last function should display a button 'get results', which, upon click, will display a new box with the score & hide #total-score
-const alabama = () => {
-    document.body.style.background = 'url(./assets/al-bg.jpg';
-    document.body.style.backgroundSize = 'cover';
-    document.getElementById('greeting-box').style.display = 'none';
-
-}
