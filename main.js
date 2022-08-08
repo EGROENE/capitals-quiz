2f4afce0a9d2e78bcfef0435856daa2fd95a9ef0
Ethan Groene <ethangroene@gmail.com>
2022-08-07 23:32:24 -0500

    Begin statesInfo array

---
+++ main.js
@@ -0,0 +1,8 @@
+// Initialize quiz score at 0:
+let totalScore = 0;
+
+// Add array of objects, each of which contain states' info:
+const statesInfo = [
+    { stateName: "Alabama", stateCapital: "Montgomery", wrongOptionOne: "Tuscaloosa", wrongOptionTwo: "Mobile", wrongOptionThree: "Huntsville" },
+    { stateName: "Alaska", stateCapital: "Juneau", wrongOptionOne: "Anchorage", wrongOptionTwo: "Barrow", wrongOptionThree: "Naknek" }
+]
