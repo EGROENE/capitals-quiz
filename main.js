580ae2b808504b180bcc72182732bbed34abd9ce
Ethan Groene <ethangroene@gmail.com>
2022-08-09 00:03:21 -0500

    Add question infrastructure for first state

--- main.js
+++ main.js
@@ -7,6 +7,45 @@ const setInitialBackground = () => {
 // Initialize quiz score at 0:
 let totalScore = 0;
 
+// Variable to count number of states answered:
+let answeredStates = 0;
+// Function to add 1 when user goes to next question:
+const addAnsweredQuestion = () => {
+    answeredStates += 1;
+}
+
+let options = document.getElementsByClassName("option");
+let correctOption = document.getElementById('correct-option');
+let wrongOptions = document.getElementsByClassName('wrong-option');
+
+// Function to disable all buttons (to be run upon click of any and all options):
+const disableButtons = () => {
+    for (const option of options) {
+        option.disabled = true;
+    }
+}
+
+// FUNCTIONS REGARDING TOTAL SCORE:
+// Function to add point if correct option is selected (run on click of this button):
+const ifCorrect = () => {
+    for (const option of options) {
+        option.disabled = true;
+    }
+    totalScore += 1;
+    document.getElementById('total-score').innerHTML = 'That\'s correct! Total for the round is: ' + totalScore + ' / ' + answeredStates;
+
+}
+
+// Function to add zero to point total if any wrong option is selected (run on click of these buttons):
+const ifWrong = () => {
+    for (const option of options) {
+        option.disabled = true;
+    }
+    totalScore += 0;
+    document.getElementById('total-score').innerHTML = 'Nope! Total score for the round is: ' + totalScore + ' / ' + answeredStates;
+}
+
+// REGARDING STATES' INFO:
 // Add array of objects, each of which contain states' info:
 const statesInfo = [
     { stateName: "Alabama", stateCapital: "Montgomery", wrongOptionOne: "Tuscaloosa", wrongOptionTwo: "Mobile", wrongOptionThree: "Huntsville" },
@@ -16,18 +55,16 @@ const statesInfo = [
 // Add a function for each state (as the correct answer will be in a different place in the order of options) that hides the previous state's function & displays the current one and updates the point total accordingly.
 // First function should hide #greeting-box; last function should display a button 'get results', which, upon click, will display a new box with the score & hide #total-score
 const alabama = () => {
+    addAnsweredQuestion();
     document.body.style.background = 'url(./assets/al-bg.jpg';
     document.body.style.backgroundSize = 'cover';
     document.getElementById('greeting-box').style.display = 'none';
     document.getElementById('display-question').innerHTML += 
         '<header>What\'s the capital of ' + statesInfo[0].stateName + '?' + '</header>'
-        + '<button class="wrong-option">' + statesInfo[0].wrongOptionThree + '</button>'
-        + '<button class="wrong-option">' + statesInfo[0].wrongOptionTwo + '</button>'
-        + '<button class="wrong-option">' + statesInfo[0].wrongOptionOne + '</button>'
-        + '<button class="correct-option">' + statesInfo[0].stateCapital + '</button>'
-        + '<div>' + totalScore + '</div>'
-    let correctOption = document.getElementsByClassName('correct-option');
-    let wrongOption = document.getElementsByClassName('wrong-option');
-    // Add button-click if statement(s) here:
-    document.getElementById('total-score').innerHTML = totalScore;
+        + '<button class="option wrong-option" onclick=ifWrong()>' + statesInfo[0].wrongOptionThree + '</button>'
+        + '<button class="option wrong-option" onclick=ifWrong()>' + statesInfo[0].wrongOptionTwo + '</button>'
+        + '<button class="option wrong-option" onclick=ifWrong()>' + statesInfo[0].wrongOptionOne + '</button>'
+        + '<button class= "option" id="correct-option" onclick=ifCorrect()>' + statesInfo[0].stateCapital + '</button>'
+    document.getElementById("reset-next-btns").innerHTML += '<button onclick="window.location.href=window.location.href">Reset Game</button>    ';
+    document.getElementById("reset-next-btns").innerHTML += '<button>Next</button>';
 }
