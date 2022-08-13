99f414ec739ecda4485a1dec41381fa9f0cd2bfb
Ethan Groene <ethangroene@gmail.com>
2022-08-12 23:29:30 -0500

    Randomize MN

--- main.js
+++ main.js
@@ -1036,6 +1036,15 @@ const minnesota = () => {
     // Add one to total of answeredStates variable:
     addAnsweredQuestion();
 
+    // Increase statesInfoIndex by 1:
+    incrementStatesInfoIndex();
+
+    // Empty stateOptions array (so that each state only contains four options & no options from other states):
+    emptyStateOptions();
+
+    // Function to randomize options:
+    randomizeOptions(statesInfoIndex);
+
     // Change background, any additional styling:
     document.body.style.background = 'url(./assets/mn-bg.jpg';
     document.body.style.backgroundSize = 'cover';
@@ -1045,10 +1054,10 @@ const minnesota = () => {
         '<section id="display-question-minnesota" class="display-question">'
         + '<header>' + answeredStates + ' / 50</header>'
         + '<header>What\'s the capital of ' + statesInfo[22].stateName + '?' + '</header>'
-        + '<button class="option wrong-option" onclick=ifWrong();enableNextBtn()>' + statesInfo[22].wrongOptionOne + '</button>'
-        + '<button class="option wrong-option" onclick=ifWrong();enableNextBtn()>' + statesInfo[22].wrongOptionTwo + '</button>'        
-        + '<button class="option wrong-option" onclick=ifWrong();enableNextBtn()>' + statesInfo[22].wrongOptionThree + '</button>'
-        + '<button class= "option" id="correct-option" onclick=ifCorrect();enableNextBtn()>' + statesInfo[22].stateCapital + '</button>'       
+        + stateOptions[0]
+        + stateOptions[1]
+        + stateOptions[2]
+        + stateOptions[3]     
         + '<div class="total-score"></div>'
         + '<div id="reset-next-btns">'
         + '<button class="reset-btn" onclick="window.location.href=window.location.href">Reset Game</button>'
