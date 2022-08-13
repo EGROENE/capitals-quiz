624078d01a7f66d83af4621458b887290ecbb630
Ethan Groene <ethangroene@gmail.com>
2022-08-12 23:10:33 -0500

    Randomize IL

--- main.js
+++ main.js
@@ -599,7 +599,7 @@ const idaho = () => {
     // Increase statesInfoIndex by 1:
     incrementStatesInfoIndex();
 
-    // Empty stateOptions array (so that each state onlycontains    four options & no options from other states):
+    // Empty stateOptions array (so that each state only contains four options & no options from other states):
     emptyStateOptions();
 
     // Function to randomize options:
@@ -636,6 +636,15 @@ const illinois = () => {
     // Add one to total of answeredStates variable:
     addAnsweredQuestion();
 
+    // Increase statesInfoIndex by 1:
+    incrementStatesInfoIndex();
+
+    // Empty stateOptions array (so that each state only contains four options & no options from other states):
+    emptyStateOptions();
+    
+    // Function to randomize options:
+    randomizeOptions(statesInfoIndex);
+
     // Change background, any additional styling:
     document.body.style.background = 'url(./assets/il-bg.jpg';
     document.body.style.backgroundSize = 'cover';
@@ -645,10 +654,10 @@ const illinois = () => {
         '<section id="display-question-illinois" class="display-question">'
         + '<header>' + answeredStates + ' / 50</header>'
         + '<header>What\'s the capital of ' + statesInfo[12].stateName + '?' + '</header>'
-        + '<button class="option wrong-option" onclick=ifWrong();enableNextBtn()>' + statesInfo[12].wrongOptionThree + '</button>'
-        + '<button class="option wrong-option" onclick=ifWrong();enableNextBtn()>' + statesInfo[12].wrongOptionTwo + '</button>'
-        + '<button class="option wrong-option" onclick=ifWrong();enableNextBtn()>' + statesInfo[12].wrongOptionOne + '</button>'
-        + '<button class= "option" id="correct-option" onclick=ifCorrect();enableNextBtn()>' + statesInfo[12].stateCapital + '</button>'       
+        + stateOptions[0]
+        + stateOptions[1]
+        + stateOptions[2]
+        + stateOptions[3]
         + '<div class="total-score"></div>'
         + '<div id="reset-next-btns">'
         + '<button class="reset-btn" onclick="window.location.href=window.location.href">Reset Game</button>'
