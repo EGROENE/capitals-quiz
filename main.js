2359f53958c69f1f71ea8bbad087b086f5e9a2e9
Ethan Groene <ethangroene@gmail.com>
2022-08-13 11:39:37 -0500

    Add 'Get Results!' btn to wyoming()

--- main.js
+++ main.js
@@ -2152,7 +2152,7 @@ const wyoming = () => {
         + '<div class="total-score"></div>'
         + '<div id="reset-next-btns">'
         + '<button class="reset-btn" onclick="window.location.href=window.location.href">Reset Game</button>'
-        + '<button class="next-btn">Next</button>'
+        + '<button class="next-btn">Get Results!</button>'
         + '</div>'
         '</section>'
 
