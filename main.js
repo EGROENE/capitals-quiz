cdf87c998d203f257d823a30cd1f7ea57d8307b0
Ethan Groene <ethangroene@gmail.com>
2022-08-13 00:04:16 -0500

    Randomize RI

--- main.js
+++ main.js
@@ -1676,6 +1676,15 @@ const rhodeIsland = () => {
     // Add one to total of answeredStates variable:
     addAnsweredQuestion();
 
+    // Increase statesInfoIndex by 1:
+    incrementStatesInfoIndex();
+
+    // Empty stateOptions array (so that each state only contains four options & no options from other states):
+    emptyStateOptions();
+
+    // Function to randomize options:
+    randomizeOptions(statesInfoIndex);
+
     // Change background, any additional styling:
     document.body.style.background = 'url(./assets/ri-bg.jpg';
     document.body.style.backgroundSize = 'cover';
@@ -1684,11 +1693,11 @@ const rhodeIsland = () => {
     document.getElementById('hero').innerHTML += 
         '<section id="display-question-rhode-island" class="display-question">'
         + '<header>' + answeredStates + ' / 50</header>'
-        + '<header>What\'s the capital of ' + statesInfo[38].stateName + '?' + '</header>'
-        + '<button class="option wrong-option" onclick=ifWrong();enableNextBtn()>' + statesInfo[38].wrongOptionOne + '</button>'
-        + '<button class="option wrong-option" onclick=ifWrong();enableNextBtn()>' + statesInfo[38].wrongOptionTwo + '</button>'
-        + '<button class= "option" id="correct-option" onclick=ifCorrect();enableNextBtn()>' + statesInfo[38].stateCapital + '</button>'      
-        + '<button class="option wrong-option" onclick=ifWrong();enableNextBtn()>' + statesInfo[38].wrongOptionThree + '</button>'        
+        + '<header>What\'s the capital of ' + statesInfo[statesInfoIndex].stateName + '?' + '</header>'
+        + stateOptions[0]
+        + stateOptions[1]
+        + stateOptions[2]
+        + stateOptions[3]        
         + '<div class="total-score"></div>'
         + '<div id="reset-next-btns">'
         + '<button class="reset-btn" onclick="window.location.href=window.location.href">Reset Game</button>'
